c2a294ef5181e46fcd9032da3d33196feefd0183
Jakub Grad <jakub.grad@icloud.com>
2025-04-06 20:20:07 +0300

    Change names of columns and tables

--- mysite/initialize_database.py
+++ mysite/initialize_database.py
@@ -7,45 +7,17 @@ db = \
 """
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE Users (
+CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
+    username TEXT,
     password TEXT,
     blog TEXT
 );
-INSERT INTO Users (name,password,blog) VALUES('Kuba','password','The text of my blog');
-INSERT INTO Users (name,password,blog) VALUES('Gecko','password','Gex, havent written anything');
-INSERT INTO Users (name,password,blog) VALUES('Robocod','password','James Pond');
-INSERT INTO Users (name,password,blog) VALUES('Fox','password','Sasha Nein');
-INSERT INTO Users (name,password,blog) VALUES('Riddle','password','Voldemort');
-COMMIT;
-"""
-
-if os.path.exists('bloggify.sqlite'):
-	print('bloggify.sqlite exists')
-else:
-	conn = sqlite3.connect('bloggify.sqlite')
-	conn.cursor().executescript(db)
-	conn.commit()
-
-import sqlite3
-import os
-
-db = \
-"""
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE Users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    password TEXT,
-    blog TEXT
-);
-INSERT INTO Users (name,password,blog) VALUES('Kuba','password','The text of my blog');
-INSERT INTO Users (name,password,blog) VALUES('Gecko','password','Gex, havent written anything');
-INSERT INTO Users (name,password,blog) VALUES('Robocod',password','James Pond');
-INSERT INTO Users (name,password,blog) VALUES('Fox','password','Sasha Nein');
-INSERT INTO Users (name,password,blog) VALUES('Riddle','password','Voldemort');
+INSERT INTO users (username,password,blog) VALUES('Kuba','password','The text of my blog');
+INSERT INTO users (username,password,blog) VALUES('Gecko','password','Gex, havent written anything');
+INSERT INTO users (username,password,blog) VALUES('Robocod','password','James Pond');
+INSERT INTO users (username,password,blog) VALUES('Fox','password','Sasha Nein');
+INSERT INTO users (username,password,blog) VALUES('Riddle','password','Voldemort');
 COMMIT;
 """
 
